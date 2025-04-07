e6b5654dc6f08466436ccc2105c6d02f45824f2f
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-07 18:35:16 +0800

    used lstrip() to remove leading spaces

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -4,4 +4,6 @@
 full_name = input("Please input your full name with spaces in the beginning: ")
 
 # Remove leading spaces using .lstrip()
+full_name = full_name.lstrip()
+
 # Print the input without the spaces in the beginning.
