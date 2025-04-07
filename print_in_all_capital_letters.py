17e2e4d4fbf3fd5626757c5eb0be985b0ce6fcff
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-07 19:03:08 +0800

    added psuedocode

---
+++ print_in_all_capital_letters.py
@@ -0,0 +1,4 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+# Ask user to input their full name
+# Format the user's input into all capital letters.
+# Print the formatted output.
