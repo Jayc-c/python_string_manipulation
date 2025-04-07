35ddf94285ac5d6a572e3837caaf90b0cc3c3e33
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-07 19:04:28 +0800

    prompted user's input

--- print_in_all_capital_letters.py
+++ print_in_all_capital_letters.py
@@ -1,4 +1,7 @@
 # Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
 # Ask user to input their full name
+full_name = input("Enter full name: ")
+
 # Format the user's input into all capital letters.
 # Print the formatted output.
