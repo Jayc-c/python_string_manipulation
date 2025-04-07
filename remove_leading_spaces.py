fa81d1fd4a7872d3da189aba8d7570a57dfc62ed
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-07 18:32:55 +0800

    prompt user input

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -1,4 +1,7 @@
 # Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. 
+
 # Ask user to input full name with spaces before the full name
+full_name = input("Please input your full name with spaces in the beginning: ")
+
 # Remove leading spaces using .lstrip()
 # Print the input without the spaces in the beginning.
