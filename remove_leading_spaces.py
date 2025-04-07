1968a0b3bf7221911a0e7bd5ef5bc404abe5cee7
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-07 18:29:15 +0800

    added psuedocode

---
+++ remove_leading_spaces.py
@@ -0,0 +1,4 @@
+# Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. 
+# Ask user to input full name with spaces before the full name
+# Remove leading spaces using .lstrip()
+# Print the input without the spaces in the beginning.
