f60033dd503b8fab0c1a59c5798fe97872826e86
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-09 20:40:32 +0800

    added pseudocode

---
+++ print_in_all_lower_case.py
@@ -0,0 +1,4 @@
+# Prog04: Create a program that ask the user to input their full name. Print the input in all lower case.
+# Prompt user to input their full name.
+# Format the user's input into all capital letters.
+# Print the formatted output.
