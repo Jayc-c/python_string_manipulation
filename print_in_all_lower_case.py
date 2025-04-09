5fdf13299a6185841436bc937b5c4bf2167ea069
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-09 20:41:58 +0800

    prompt user input

--- print_in_all_lower_case.py
+++ print_in_all_lower_case.py
@@ -1,4 +1,7 @@
 # Prog04: Create a program that ask the user to input their full name. Print the input in all lower case.
+
 # Prompt user to input their full name.
+full_name = input("Enter full name: ")
+
 # Format the user's input into all capital letters.
 # Print the formatted output.
