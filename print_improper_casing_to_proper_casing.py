cd2e15101f24f90b35a75382368b3eab339b0a72
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-09 21:02:30 +0800

    prompt user input

--- print_improper_casing_to_proper_casing.py
+++ print_improper_casing_to_proper_casing.py
@@ -1,4 +1,7 @@
 # Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
 # Prompt user to input their full name.
+full_name = input("Enter full name: ")
+
 # Format the incorrect casing into proper casing.
 # Print formatted input.
