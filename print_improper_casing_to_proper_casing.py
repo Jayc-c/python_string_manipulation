4df0668f40062e190ab927d20a5fcbd2f62acacc
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-04-09 21:01:05 +0800

    added psuedocode

---
+++ print_improper_casing_to_proper_casing.py
@@ -0,0 +1,4 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+# Prompt user to input their full name.
+# Format the incorrect casing into proper casing.
+# Print formatted input.
