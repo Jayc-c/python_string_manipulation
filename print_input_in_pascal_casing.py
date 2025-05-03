0df957d06ed0827f445c44141a264ec2ce87b730
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-05-03 12:38:50 +0800

    added pseudocode

---
+++ print_input_in_pascal_casing.py
@@ -0,0 +1,4 @@
+# Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+# Ask the user to input their full name in incorrect casing.
+# Convert to title case, remove spaces to make PascalCase.
+# Print the formatted output in pascal casing.
