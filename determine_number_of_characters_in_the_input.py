cd73e86fbd14a6f535367609656be9309a74a542
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-05-03 12:26:42 +0800

    prompt user input

--- determine_number_of_characters_in_the_input.py
+++ determine_number_of_characters_in_the_input.py
@@ -1,4 +1,7 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
 # Ask the user to input their full name. 
+full_name = input("Enter your full name: ")
+
 # Count the number of characters in the input (including spaces).
 # Print the number of characters.
