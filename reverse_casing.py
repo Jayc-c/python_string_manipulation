09bd439e548891ce630795bc64ddc24c3f2256e7
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-05-03 12:00:51 +0800

    added pseudocode

---
+++ reverse_casing.py
@@ -0,0 +1,4 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+# Ask the user to input their name in incorrect casing.
+# Format the user's input into their reverse caising format.
+# Print the formatted output.
