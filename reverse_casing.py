a23928cfe5629b6d4b5f3b904c05e97c3fb13bd3
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-05-03 12:02:41 +0800

    prompt user input

--- reverse_casing.py
+++ reverse_casing.py
@@ -1,4 +1,7 @@
 # Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
 # Ask the user to input their name in incorrect casing.
+full_name = input("Input your full name in incorrect casing: ")
 # Format the user's input into their reverse caising format.
+
 # Print the formatted output.
