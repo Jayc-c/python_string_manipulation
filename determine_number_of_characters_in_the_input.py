b0eba4939e0d64f62e1d6c28e47bfc7eecb1e1de
John Carlo Vasquez <johncarlovasquez0527@gmail.com>
2025-05-03 12:25:56 +0800

    added pseudocode

---
+++ determine_number_of_characters_in_the_input.py
@@ -0,0 +1,4 @@
+# Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+# Ask the user to input their full name. 
+# Count the number of characters in the input (including spaces).
+# Print the number of characters.
